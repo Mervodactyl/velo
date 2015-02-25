b910b169cb8f3323eaa8ce74a587ad5bd381a0e7
Mervodactyl <merve.silk@gmail.com>
2015-02-25 13:09:26 +0000

    Docking station capacity set and passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,9 @@ require 'docking_station'
 describe DockingStation do
 
   let(:bike)    { Bike.new }
-  let(:station) { DockingStation.new }
+  let(:station) { DockingStation.new(capacity: 20) }
 
-  it "should be able to accept a Bike" do
+  it "is able to accept a Bike" do
     expect(station.bike_count).to eq(0)
     station.dock(bike)
     expect(station.bike_count).to eq(1)
@@ -17,4 +17,19 @@ describe DockingStation do
     station.release_bike(bike)
     expect(station.bike_count).to eq(0)
   end
+
+  it "knows when it is full" do
+    expect(station).not_to be_full
+    20.times { station.dock(bike) }
+    expect(station).to be_full
+  end
 end
+
+
+
+
+
+
+
+
+
