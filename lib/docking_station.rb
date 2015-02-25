c030aca6bee2d395ed345c9514d1b1a4bd82cdb3
Mervodactyl <merve.silk@gmail.com>
2015-02-25 13:09:26 +0000

    testing old message commit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,9 @@
 class DockingStation
 
-  def initialize
+  DEFAULT_CAPACITY = 20
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY) ## want to talk in person about this passing a hash as an argument....
     @bikes =  []
   end
 
@@ -9,11 +12,16 @@ class DockingStation
   end
 
   def dock(bike)
-    @bikes << bike
+    raise "Station is FULL, cannot accept Bike" if full? ## why wont it work the other way round, ie 'if full? raise..'
+    @bikes << bike ## can I put this line first? if not, why not? surely you try to dock bike first, and then find out through doing so that it wont work
   end
 
   def release_bike(bike)
-    @bikes.delete(bike)
+    @bikes.delete(bike) ## why is there no such thing as a '>>' ie. reverse shovel?
+  end
+
+  def full?
+    bike_count == @capacity
   end
 
 end
